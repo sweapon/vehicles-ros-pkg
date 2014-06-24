35b25a2f442fc762e1eacbb04bb124bbc9b1d2dc
Ivor Rendulic <ivor.rendulic@gmail.com>
2014-06-24 15:44:08 +0200

    ConstPtr type

--- pladypos/src/BatteryAlarmNode.cpp
+++ pladypos/src/BatteryAlarmNode.cpp
@@ -8,14 +8,13 @@ using namespace labust::vehicles;
 BatteryAlarmNode::BatteryAlarmNode() :
     threshold_(18) {
   ros::NodeHandle nh;
-  nh.getParam("threshold", threshold_);
   battery_voltage_subscriber = nh.subscribe("battery_voltage", 1, &BatteryAlarmNode::checkVoltage, this);
   alarm_publisher = nh.advertise<std_msgs::Bool>("battery_alarm", 1);
 }
 
 BatteryAlarmNode::~BatteryAlarmNode() {}
 
-void BatteryAlarmNode::checkVoltage(const std_msgs::Float32Ptr battery_voltage) {
+void BatteryAlarmNode::checkVoltage(const std_msgs::Float32::ConstPtr& battery_voltage) {
   if ( battery_voltage->data < threshold_) {
     std_msgs::Bool alarm;
     alarm.data = true;
