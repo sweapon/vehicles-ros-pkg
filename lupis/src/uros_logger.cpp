19211c290a6694dc8ee22693058a56f2d3d199c3
gibo1990 <filip.mandic@gmail.com>
2014-09-23 10:34:14 +0200

    UROS logger field changes

--- lupis/src/uros_logger.cpp
+++ lupis/src/uros_logger.cpp
@@ -1,9 +1,44 @@
-/*
+/*********************************************************************
  * uros_logger.cpp
  *
  *  Created on: Sep 3, 2014
  *      Author: Filip Mandic
- */
+ *
+ ********************************************************************/
+
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2014, LABUST, UNIZG-FER
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the LABUST nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
 
 #include <iostream>
 #include <fstream>
@@ -13,14 +48,19 @@
 
 #include <ros/ros.h>
 
+#include <std_msgs/Float32.h>
+#include <auv_msgs/NavSts.h>
+
 class UROSLogger{
 
 public:
 
-	UROSLogger(){
+	UROSLogger():rhodamineData(0.0){
 
 		ros::NodeHandle nh;
 
+		subRhodamineData = nh.subscribe<std_msgs::Float32>("adc", 1, &UROSLogger::onRhodamineData, this);
+		subPositionData = nh.subscribe<auv_msgs::NavSts>("state_out",1, &UROSLogger::onPositionData, this);
 
 	}
 
@@ -56,11 +96,10 @@ public:
 			log_file << "%Time (unix time), Latitude (decimal degree), Longitude(decimal degree), Depth (m), Rhodamine (ppb),Crude, Refined" << std::endl;
 
 			// Test line
-			std::time_t t = std::time(0);
-			ss.str(std::string());
-			ss << t;
-
-			log_file << ss.str() << std::endl;
+//			std::time_t t = std::time(0);
+//			ss.str(std::string());
+//			ss <<t<<","<<latLonData.global_position.latitude<<","<<latLonData.global_position.longitude<<","<<latLonData.position.depth<<","<<rhodamineData<<",0,0";
+//			log_file << ss.str() << std::endl;
 
 			return true;
 		} else {
@@ -68,9 +107,30 @@ public:
 		}
 	}
 
+	void writeLog(){
+
+		std::stringstream ss;
+		ss <<std::time(0)<<","<<latLonData.global_position.latitude<<","<<latLonData.global_position.longitude<<","<<latLonData.position.depth<<","<<rhodamineData<<",0,0";
+		log_file << ss.str() << std::endl;
+
+	}
+
+	void onRhodamineData(const std_msgs::Float32::ConstPtr& data){
+		rhodamineData = data->data;
+	}
+
+	void onPositionData(const auv_msgs::NavSts::ConstPtr& data){
+		latLonData = *data;
+		if(log_file.is_open())
+			writeLog();
+	}
+
 	std::ofstream log_file;
 
-	ros::Subscriber subRhodamineData;
+	ros::Subscriber subRhodamineData, subPositionData;
+
+	double rhodamineData;
+	auv_msgs::NavSts latLonData;
 
 
 };
