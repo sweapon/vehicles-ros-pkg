f36c72e0bfe6003d8ae633ea17d33474595184b9
gibo1990 <filip.mandic@gmail.com>
2014-09-23 12:31:44 +0200

    Changed save folder

--- lupis/src/uros_logger.cpp
+++ lupis/src/uros_logger.cpp
@@ -75,7 +75,7 @@ public:
 		/* Generate log filename */
 		boost::posix_time::time_facet *facet = new boost::posix_time::time_facet("%Y-%m-%d_%H-%M");
 		ss.imbue(std::locale(ss.getloc(), facet));
-		ss << "/home/stdops/lauv-lupis-2/logs/log_" << boost::posix_time::second_clock::universal_time() << ".csv";
+		ss << "/home/stdops/logs/lauv-lupis-2/log_" << boost::posix_time::second_clock::universal_time() << ".csv";
 
 		/* Open log file */
 		log_file.open(ss.str().c_str(), std::ios::out);
