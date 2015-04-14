2f26c280df99059645ae7756c9447a57b4a25c2a
stdops-pladypos <dula.nad@gmail.com>
2015-04-14 10:47:45 +0200

    Added pladypos updates.

--- pladypos/data/upstart/env.bash
+++ pladypos/data/upstart/env.bash
@@ -4,6 +4,7 @@ USER=stdops
 VEHICLE=pladypos
 ROS_HOME=/home/${USER}/ros
 source ${ROS_HOME}/devel/setup.bash
+export ROS_MASTER_URI=http://localhost:11311
 
 # Vehicle configuration environment
 source `rospack find ${VEHICLE}`/data/devices/device_config.bash
@@ -17,3 +18,4 @@ LAUNCH_FILE=pladypos_standard_mws.launch
 # Configure logging
 LOG_PATH=/extern/launcher
 ROS_LOG_PATH=/extern/ros
+LOGGING=false
