531e05b8f932506734aa1a05a8c9404f5591381f
PlaDyPos <labust-unizg@gmail.com>
2015-04-30 17:01:27 +0200

    Extended the pladypos configurations with new nodes and corrected the device configuration.

--- pladypos/data/devices/device_config.bash
+++ pladypos/data/devices/device_config.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Ublox configuration
-export UBLOX_PORT=/dev/ttyACM1
+export UBLOX_PORT=/dev/ttyACM0
 export UBLOX_BAUD=115200
 export UBLOX_USE_GPS=true
 
@@ -16,7 +16,7 @@ export NAVQUEST_FIXED_YAW=3.14159
 export NAVQUEST_USE_FIXED_YAW=true
 
 # Seatrac USBL configuration
-export USBL_PORT=/dev/ttyS0
+export USBL_PORT=/dev/ttyUSB2
 export USBL_BAUD=115200
 
 # Novatel dGPS configuration
@@ -25,7 +25,7 @@ export NOVATEL_BAUD=115200
 export USE_NOVATEL_GPS=false
 
 # Arduino driver configuration
-export ARDUINO_PORT=/dev/ttyACM0
+export ARDUINO_PORT=/dev/ttyACM1
 export ARDUINO_BAUD=115200
 
 # VideoRay driver configuration
@@ -35,3 +35,5 @@ export VR_BAUD=115200
 # Relay Module configuration
 export RELAY_MODULE_IP=pladypos-eth
 export RELAY_MODULE_PORT=17494
+export RELAY_MODULE2_IP=pladypos-eth2
+export RELAY_MODULE2_PORT=17494
